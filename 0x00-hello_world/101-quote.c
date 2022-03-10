a1781923e66ed45328f93427e1af2dcc1bf37ce4
Buruk Asrat <burukasrat@gmail.com>
2022-03-11 00:54:54 +0300

    print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - this is the main funciton
+ * Return: 1 Always
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
