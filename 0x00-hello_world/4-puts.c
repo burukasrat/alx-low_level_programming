0c230aa3fb5361f0d54a028141dfd23ee563d181
Buruk Asrat <burukasrat@gmail.com>
2022-03-11 00:13:33 +0300

    at

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
-/* main - this is the main function */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+/* *
+ * 
+ * main - this is the main function
+ *
+ * */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle\n");
+        return (0);
 }
