7ad8035532e6b37225396c29b2a04f07499bac55
Buruk Asrat <burukasrat@gmail.com>
2022-03-11 00:44:11 +0300

    river

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 /**
  * main - this is the main function
+ * Return: 0
  */
-int main(void)/* return: 0 */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
