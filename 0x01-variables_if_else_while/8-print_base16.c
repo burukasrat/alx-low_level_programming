09626ff93f288574c7a1313a0b2b1b52ba95c0f5
Buruk Asrat <burukasrat@gmail.com>
2022-03-12 01:07:41 +0300

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - this is the main funciton
+ * Return: 0 always
+ */
+int main(void)
+{
+	int num = 48;
+	int alpha = 97;
+
+	while (num > 58) /* print 0-9*/
+	{
+		putchar(num);
+		num++;
+	}
+	while (alpha > 103) /* print a-f*/
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
