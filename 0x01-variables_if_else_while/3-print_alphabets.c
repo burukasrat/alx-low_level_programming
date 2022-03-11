02fd2a38c4f2395e0df01b77154b08262f3cf9d4
Buruk Asrat <burukasrat@gmail.com>
2022-03-12 00:24:22 +0300

    alphabet upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - the main function
+ * Return: 0 always
+ */
+int main(void)
+{
+        int alphaU = 97;
+
+        while (alphaU < 123) /* print the alphabet using putchar */
+        {
+                putchar(alphaU);
+                alphaU++;
+        }
+	int alphaL = 65;
+
+	while (alphaL < 91) /* print lowercase alphabets*/
+	{
+		putchar(alphaL);
+		alphaL++;
+	}
+        putchar('\n');
+        return (0);
+}
+
