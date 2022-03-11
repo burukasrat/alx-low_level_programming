514aaa0cfaad9b092f6c20f320d90eb61cc7e58c
Buruk Asrat <burukasrat@gmail.com>
2022-03-12 00:46:51 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - this is the main function
+ * Return: 0 always
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
