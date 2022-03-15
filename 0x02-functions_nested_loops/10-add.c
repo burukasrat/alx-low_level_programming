fad42dcb4dc00145a3399cba8137b28dc49097c9
Buruk Asrat <burukasrat@gmail.com>
2022-03-16 00:55:35 +0300

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two ints
+ * Return: sum 
+ * @a: first element
+ * @b: second element
+ */
+int add(int a, int b)
+{
+	return(a+b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * times_table - hotline
+ * Return: void
+ */
+void times_table(void)
+{
+	int col, row, row1, col1;
+
+	row = 0;
+	
+	while (row < 10)
+	{
+		col = 0;
+		while (col < 10)
+		{
+			_putchar(
