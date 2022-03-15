505d33391b9813e3cd15f28255cecbe69f8aa6b9
Buruk Asrat <burukasrat@gmail.com>
2022-03-15 13:29:58 +0300

    hello

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,20 @@
 #include "main.h"
 /**
+ * print_alphabet - print alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+        int loop = 97;
+
+        while (loop < 123)
+        {
+                _putchar(loop);
+                loop++;
+        }
+        _putchar('\n');
+}
+/**
  * main - check the code
  * Return: always 0
  */
