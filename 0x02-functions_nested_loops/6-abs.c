eda60c3c41432c265aa8cf7ec474b2c40f69c284
Buruk Asrat <burukasrat@gmail.com>
2022-03-15 23:09:48 +0300

    who

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - absolut value
+ */
+int _abs(int)
+{
+	if (int > 0)
+	{
+		return (int);
+	}
+	else if (int < 0)
+	{
+		return ((int*(-1)));
+	}
+	else
+		return (0);
+}
