36d2061f96bb07bde89ab388f4b6bc7f8228da9d
Buruk Asrat <burukasrat@gmail.com>
2022-03-16 01:41:32 +0300

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**
+ * fibonacci - sequence
+ * Return: void
+ */
+void fibonacci(void)
+{
+	int n1 = 1, n2 = 2, sum, counter = 0;
+
+	printf("%d, %d, ", n1, n2);
+	while (counter < 48)
+	{
+		sum = n1 + n2;
+		if (n == 47)
+		{
+			printf("%d\n", sum);
+		}
+		else
+		{
+			printf("%d, ", sum);
+		}
+		n1 = n2;
+		n2 = sum;
+		counter++;
+	}
+}
+/**
+ * main - main
+ * Return: 0 always
+ */
+int main(void)
+{
+	fibonacci();
+	return (0);
+}
+
