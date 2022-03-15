8b30347fd64a11df9ff5f4840c73a0b2e8af3004
Buruk Asrat <burukasrat@gmail.com>
2022-03-15 12:33:47 +0300

    this is it

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,11 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char c[8] = {'_','p','u','t','c','h','a','r'};
+	int loop = 0;
+	while (loop < 8)
+	{
+		_putchar("%d",c[loop]);
+	}
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
-i dont
+#include<stdio.h>
+/**
+ * _putchar - prints char
+ * Return: void
+ */
+void _putchar(int c)
+{
+	putchar("%c", c);
+	return (0);
+}
+
