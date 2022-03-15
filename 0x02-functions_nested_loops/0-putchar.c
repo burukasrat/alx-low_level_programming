69e1d3959a3325e4aae366828be71731215eafb6
Buruk Asrat <burukasrat@gmail.com>
2022-03-15 13:13:26 +0300

    fight

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,9 @@
  */
 int main(void)
 {
-	char c[8] = {95 , 112 , 117 , 116 , 99 , 104 , 97 , 114};
+	char c[8] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int loop = 0;
+
 	while (loop < 8)
 	{
 		_putchar(c[loop]);
