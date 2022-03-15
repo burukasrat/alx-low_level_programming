becb862edbea35af6615e18855e2963facb6571f
Buruk Asrat <burukasrat@gmail.com>
2022-03-16 00:28:32 +0300

    love

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,19 +6,10 @@
  */
 int print_last_digit(int n)
 {
-	int last;
 	int num[10] = {48, 49, 50, 51, 52,
 		53, 54, 55, 56, 57};
 
-	if (n >= 0) /* negative */
-	{
-		last = n % 10;
-	}
-	else
-	{
-		n = n * (-1);
-		last = n % 10;
-	}
-	_putchar(num[last]);
-	return (last);
+	n = _abs(n);
+	_putchar(num[n % 10]);
+	return (n % 10);
 }
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * jack_bauer - print 24 hr
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int n1 = 48, n2, n3, n4, hr = 0;
+
+	while (hr < 24) /* print the time*/
+	{
+		while (n1 < 58)
+		{
+			_putchar(n1);
+			n2 = 48;
+			while (n2 < 58)
+			{
+				_putchar(n2);
+				n2++;
+			}
+			n1++;
+		}
+		_putchar(58);
+		n3 = 48;
+		while(n3 < 58)
+		{
+			_putchar(n3);
+			n4 = 48;
+			while (n4 < 58)
+			{
+				_putchar(n2);
+				n4++;
+			}
+			n3++;
+		}
+		hr++;
+	}
+}
+
