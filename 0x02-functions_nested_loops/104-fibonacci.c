16f26979d31fd04ca2a30f4376fc022410c4817e
Buruk Asrat <burukasrat@gmail.com>
2022-03-16 02:05:49 +0300

    so far

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * print_fibonacci - hello
+ * Return: void
+ */
+void print_fibonacci(void)
+{
+	long double int n1 = 1, n2 = 2,
+	     sum = 0, counter = 0;
+	
+	printf("%Lf, %Lf, ", n1, n2);
+	while (counter < 98)
+	{
+		sum = n1 + n2;
+		if (counter == 97)
+		{
+			printf("%Lf\n", sum);
+		}
+		else
+		{
+			printf("%Lf, ", sum);
+		}
+		counter++;
+	}
+}
+/**
+ * main - main
+ * Return: int
+ */
+int main(void)
+{
+	print_fibonacci();
+	return (0);
+}
