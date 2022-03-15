885eb4283e7221cdce2282f5b997bff9e87d850e
Buruk Asrat <burukasrat@gmail.com>
2022-03-15 21:42:00 +0300

    h

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,14 +8,14 @@ int print_sign(int n)
 {
 	if (n > 0) /* print +*/
 	{
-		return ("+1");
+		return (+1);
 	}
 	else if (n < 0) /* print -*/
 	{
-		return ("-1");
+		return (-1);
 	}
 	else /* print 0*/
 	{
-		return ("00");
+		return (00);
 	}
 }
