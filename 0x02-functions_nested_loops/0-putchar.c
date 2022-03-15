19bf68609b992b9f4203d1a0b19bfa11e4513d69
Buruk Asrat <burukasrat@gmail.com>
2022-03-15 12:59:08 +0300

    array

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	char c[8] = {'_','p','u','t','c','h','a','r'};
+	char c[8] = {95,112,117,116,99,104,97,114};
 	int loop = 0;
 	while (loop < 8)
 	{
-		_putchar("%d",c[loop]);
+		_putchar(c[loop]);
 	}
 	return (0);
 }
