7a65e8f91b408ad28fcf4a1675d3b67873d7a0a0
Buruk Asrat <burukasrat@gmail.com>
2022-03-17 16:47:59 +0300

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - checks if c is upperccase
+ * Return: 1 if upper, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c > 91) /* checks if upper */
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+this is a readme file for the project nested loops
+the files contains some c codes to practice using c loops
