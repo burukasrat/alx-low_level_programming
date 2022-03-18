e6686a88f992354d4a88e2aaa994df806e88badd
Buruk Asrat <burukasrat@gmail.com>
2022-03-19 01:58:29 +0300

    places

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 always
+ */
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((n % 3 == 0) &&
+				(n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+
+		if (n != 100)
+		{
+			printf(" ");
+		}
+		n++;
+	}
+	return (0);
+}
