e2bdc05ca2c49f70233110038b34c0127ba59cd1
Buruk Asrat <burukasrat@gmail.com>
2022-03-19 00:50:35 +0300

    i will wake up this time

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print-diagonal - print \ diagonally
+ * Return: void
+ * @n: n times
+ */
+void print_diagonal(int n)
+{
+	int i = 1, s, b = 0;
+
+	while (i < n)
+	{
+		s = 0;
+		while (s < b)
+		{
+			_putchar(32);
+			s++;
+		}
+		b++;
+		_putchar(92);
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
