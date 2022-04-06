9c163103d1c0f3c1ac0788bf01b5d6cd6683d056
Buruk Asrat <burukasrat@gmail.com>
2022-04-06 12:24:54 +0300

    this

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * char *_strdup(char *str) - creates si
+ * @str: string
+ * Return: something
+ */
+char *_strdup(char *str)
+{
+	char *s;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else 
+	{
+		return (s = str);
+	}
+}
