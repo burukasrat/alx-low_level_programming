ccfcbfe9b71e410190670b56d541a3b7f646ad8c
Buruk Asrat <burukasrat@gmail.com>
2022-04-06 11:59:46 +0300

    meant for me

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * char *create_array - creates array with assined size
+ * @size: size of array
+ * @c: name of the array
+ * Return: NULL or array
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return ("NULL");
+	}
+	else
+	{
+		return (c = malloc(size));
+	}
+}
